0a7f26197bfe4318965d1097982752e4258bd065
cybregaz <kkanttechy@gmail.com>
2026-01-23 02:14:13 +0530

    ...

--- configuration.nix
+++ configuration.nix
@@ -238,7 +238,7 @@
   };
 
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "25.05"; # Did you read the comment?
+  system.stateVersion = "26.01";
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
